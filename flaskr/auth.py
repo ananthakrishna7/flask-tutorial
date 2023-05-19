c03cfbd5fdb156dd4e5e2c1fcf52f990763b0b0b
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2023-05-20 00:02:56 +0100

    Created Jinja html templates and used the CSS file given in the tutorial.
    I can modify all this stuff to suit my needs. I can reuse this for some OTHER projects.
    Refer: https://flask.palletsprojects.com/en/2.3.x/tutorial/templates/
    And the next page. :)

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-        Blueprint, flash, g, redirect, render_template, request, session
+        Blueprint, flash, g, redirect, render_template, request, session, url_for
         )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -66,12 +66,10 @@ def login():
 def load_logged_in_user():
     user_id = session.get('user_id')
 
-    if user_is is None:
+    if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-                'SELECT * FROM user WHERE id = ?', (user_id)
-                ).fetchone()
+        g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
 
 @bp.route('/logout')
 def logout():

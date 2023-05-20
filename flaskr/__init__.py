32cf75eefbde2b075e26b544c525e53eeea4e34b
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2023-05-20 17:05:21 +0100

    I have successfuly finished writing the code for the flask app!
    We created the html pages required for the flask views to function. I learnt a lot.
    Refer: https://flask.palletsprojects.com/en/2.3.x/tutorial/blog/

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -34,5 +34,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index') # this adds a rule. this ensures that url_for('index') url_for('blog.index') build the same url: /index. it is at the root because it is the main thing in our blogging website. that's why it does not have a url prefix. refer that website for a better understanding.
+
     return app
 
